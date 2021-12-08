ba1b1d3fe3528b654f78551c086654113956ddd5
Israel Julio <israel.jhonatas@sesisenaibetim.com.br>
2021-12-08 11:12:35 -0300

    Adicionado componente 'photo'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+  endereco = 'https://images.news18.com/ibnlive/uploads/2021/08/1628511415_lion-1200x800.jpg?impolicy=website&width=510&height=356';
 }
