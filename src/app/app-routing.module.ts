7659fc0f2555da05cf6b04ba5d4f35fe385568a1
Israel Julio <israel.jhonatas@sesisenaibetim.com.br>
2021-12-09 17:20:41 -0300

    adicionada pagina de erro para rota invalida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 
 const routes: Routes = [
   { path: '', component: PhotoListComponent },
   { path: 'title', component: PhotoFormComponent},
-  { path: '**', component: PhotoListComponent}
+  { path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
  import { HttpClientModule } from '@angular/common/http';
 import { PhotosModule } from './photos/photos.module';
+// import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,7 @@ import { PhotosModule } from './photos/photos.module';
     BrowserModule,
     AppRoutingModule,
     PhotosModule,
+    ErrorsModule,
 
   ],
   providers: [],
