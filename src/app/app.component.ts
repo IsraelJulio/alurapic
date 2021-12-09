ba481133d6cdd03e70904d62d0f781d019c7d562
Israel Julio <israel.jhonatas@sesisenaibetim.com.br>
2021-12-09 14:00:20 -0300

    geração do componente photo-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,18 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { Photo } from './models/Photo.model';
-import { PhotoService } from './photos/photo/photo.service';
+// import { HttpClient } from '@angular/common/http';
+// import { Photo } from './models/Photo.model';
+// import { PhotoService } from './photos/photo/photo.service';
 
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  ngOnInit(): void {
+  }
   title = 'alurapic';
 
 
-
-  photosApi: Photo[] = [];
-
-  photos: Photo[] = [
-  // photos = [
-    {
-      url:"https://img.elo7.com.br/product/main/3722582/quadro-mosaico-4-pecas-rosto-do-leao-preto-e-branco-canvas.jpg",
-      description:"lion",
-      id:'k'
-    },
-    {
-      url:"https://img.elo7.com.br/product/main/3722582/quadro-mosaico-4-pecas-rosto-do-leao-preto-e-branco-canvas.jpg",
-      description:"lion",
-      id:'k'
-    }
-  ];
-
-  constructor(private photoService: PhotoService){}
-
-  ngOnInit(): void{
-    this.photoService.SearchPhoto().subscribe(x => this.photosApi = x)
-  }
 }
 
--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Photo } from 'src/app/models/Photo.model';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoListComponent implements OnInit {
 
-  constructor() { }
+  photosApi: Photo[] = [];
 
-  ngOnInit(): void {
+  photos: Photo[] = [
+  // photos = [
+    {
+      url:"https://img.elo7.com.br/product/main/3722582/quadro-mosaico-4-pecas-rosto-do-leao-preto-e-branco-canvas.jpg",
+      description:"lion",
+      id:'k'
+    },
+    {
+      url:"https://img.elo7.com.br/product/main/3722582/quadro-mosaico-4-pecas-rosto-do-leao-preto-e-branco-canvas.jpg",
+      description:"lion",
+      id:'k'
+    }
+  ];
+
+  constructor(private photoService: PhotoService){}
+
+  ngOnInit(): void{
+    this.photoService.SearchPhoto().subscribe(x => this.photosApi = x)
   }
-
 }
