e6801600e9766f3241b0db94a353da42948262dd
Israel Julio <israel.jhonatas@sesisenaibetim.com.br>
2021-12-09 11:07:51 -0300

    Criação de modelo para fotos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Photo } from './models/Photo.model';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'alurapic';
 
-  photos: Object[] = [];
 
-constructor(http: HttpClient){
-  http
-  .get<Object[]>('https://www.pexels.com/photo/brown-rocks-during-golden-hour-2014422/')
-  .subscribe(photo => this.photos = photo);
 
-}
+  // photos: Object[] = [];
+  photos: Photo[] = [
+  // photos = [
+    {
+      url:"https://img.elo7.com.br/product/main/3722582/quadro-mosaico-4-pecas-rosto-do-leao-preto-e-branco-canvas.jpg",
+      description:"lion"
+    },
+    {
+      url:"https://img.elo7.com.br/product/main/3722582/quadro-mosaico-4-pecas-rosto-do-leao-preto-e-branco-canvas.jpg",
+      description:"lion"
+    }
+  ];
+  url: string = "https://img.elo7.com.br/product/main/3722582/quadro-mosaico-4-pecas-rosto-do-leao-preto-e-branco-canvas.jpg";
+  description: string = "lion";
+  constructor(http: HttpClient){
+
+    // var o = http
+    // .get<Object[]>('https://www.pexels.com/photo/brown-rocks-during-golden-hour-2014422/')
+    // .subscribe(photos =>{
+
+      //   console.log(photos);
+      // this.photos = photos
+      // } );
+
+      console.log(this.photos);
+  }
 
 }
